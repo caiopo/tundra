6675eaf5744703790d27410c6b1ba308b733acba
caiopo <caiopoliveira@gmail.com>
2017-07-19 12:17:31 -0300

    Improve constant test strings

--- test/test_util.py
+++ test/test_util.py
@@ -7,15 +7,16 @@ from context import Graph, export_dot, export_png, to_dot
 g_dot1 = 'graph .* {\n1 -- 2;\n0 -- 1;\n5 -- 6;\n' \
     '6 -- 7;\n8 -- 9;\n3 -- 4;\n7 -- 8;\n4 -- 5;\n2 -- 3;\n}'
 
-
 g_dot2 = 'graph .* {\n1 -- 2;\n0 -- 1;\n5 -- 6;\n' \
          '6 -- 7;\n3 -- 4;\n7 -- 8;\n4 -- 5;\n2 -- 3;\n9;}'
 
-g_dot_weighted = 'graph {} {{\n2 -- 3 [label="5"];' \
-    '\n7 -- 8 [label="5"];\n8 -- 9 [label="5"];' \
-    '\n6 -- 7 [label="5"];\n4 -- 5 [label="5"];' \
-    '\n5 -- 6 [label="5"];\n3 -- 4 [label="5"];' \
-    '\n1 -- 2 [label="5"];\n0 -- 1 [label="5"];\n}}'
+g_dot_weighted = '''
+graph {} {{\n2 -- 3 [label="5"];\
+\n7 -- 8 [label="5"];\n8 -- 9 [label="5"];\
+\n6 -- 7 [label="5"];\n4 -- 5 [label="5"];\
+\n5 -- 6 [label="5"];\n3 -- 4 [label="5"];\
+\n1 -- 2 [label="5"];\n0 -- 1 [label="5"];\n}}
+'''.strip()
 
 
 def is_png(filename):
